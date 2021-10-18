01d721eae4ed02bfd8f9f57e768114af7f5e2eb0
Joseph <59439026+JosephKBS@users.noreply.github.com>
2021-10-17 21:48:50 -0400

    Update w4_exercise_solutions.R

--- w4_exercise_solutions.R
+++ w4_exercise_solutions.R
@@ -3,10 +3,8 @@
 ### 0. import data
 df <- read.csv("find_my_apartment.csv")
 
-
 ### 1. create density plots of all rent
 plot(density(df$rent))
-
 
 ### 2-1. preconditions: 
 ### first, create a column computing actual rent (actual rent =rent + utility + wifi)
@@ -19,7 +17,6 @@ df$actual <- NA     # creating empty column called "actual"
 for (i in nrow(df)){
   df$actual[i] <- df$rent[i] + df$utility[i] + df$wifi[i]
 }
-
 
 
 ### 2-2. preconditions
@@ -35,7 +32,6 @@ for (i in nrow(df) ){
 }
 
 
-
 ### 2-2 Using for loop and conditionals to find..
   # (a) Rent < 2400($)
   # (b) Room type is either fine
@@ -43,8 +39,6 @@ for (i in nrow(df) ){
   # (d) No higher than 5th floor
   # (e) Grocery in 10 minutes
   # (f) Café near 10 minutes
-
-
 
 # 1. one line code (Basic R code version)
 df[ which( df$rent < 2400 & df$floor < 5 & df$grocery<10 & df$cafe < 10) , ]
