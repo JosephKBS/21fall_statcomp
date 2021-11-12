24b3fcd72fe6eeb0448aca1d2c9271fcf7d5bbb4
Joseph <59439026+JosephKBS@users.noreply.github.com>
2021-11-12 14:55:22 -0500

    Update w10_exercises.R

--- w10/w10_exercises.R
+++ w10/w10_exercises.R
@@ -1,6 +1,9 @@
 # Week 10 exercises
 
-
+# ---------------------------------
+# 1. finding "Lea"
+# Exercise source: https://discdown.org/rprogramming/loops.html
+# ---------------------------------
 
 first_name <- c("Lea",     "Sabine", "Mario", "Lea", "Peter",   "Max")
 last_name  <- c("Schmidt", "Gross",  "Super", "Kah", "Steiner", "Muster")
@@ -11,4 +14,33 @@ for (i in seq_along(first_name)) {
     if (first_name[i] == "Lea") {
         print(paste("Found:", first_name[i], last_name[i]))
     }
+}
+
+# ---------------------------------
+# 2. Calculating function
+# ---------------------------------
+# function
+calculate_funct <- function(x) {
+        ifelse(x < 0, x^4 + 4*x^2 - 17*x, 
+               ifelse(x <= 3, x+10,
+                      -x^2 + 2*x - 10))
+    }
+# random x values
+random_x <- seq(-2, 3, len=10)
+
+# plot
+plot(random_x, calculate_funct(random_x), type="l")
+
+# ---------------------------------
+# 3. Matrix calculation
+# ---------------------------------
+matrix_funct <- function(mat) {
+    # if x>10 or x <0, then x= 0
+    mat[mat > 10 | mat < 0] <- 0 * mat[mat > 10 | mat < 0]
+    # if x is even number, then x = x^3
+    mat[mat%%2 == 1] <-  mat[mat%%2 == 1]^3
+    # if x is odd number, then x= x*2
+    mat[mat%%2 != 1] <-  mat[mat%%2 == 1]^2
+    # print
+    mat 
 }
